a5d772177612af2419658e39092da3418c63d20e
mekane <4631242+mekane84@users.noreply.github.com>
2025-09-01 14:43:51 -0500

    first commit

---
+++ src/test/java/com/example/springboot2025idea/SpringBoot2025IdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot2025idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2025IdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
